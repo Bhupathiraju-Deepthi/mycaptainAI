310e612d9ad6e3d3a6725d8bf4eb55484f3e5a7f
Bhupathiraju Deepthi <97110722+Bhupathiraju-Deepthi@users.noreply.github.com>
2022-09-16 03:24:18 +0530

    Create setop.py

---
+++ setop.py
@@ -0,0 +1,14 @@
+e={0,1,2,3,4}
+n={4,5,6,8,2}
+# set union operation
+print("union of e and n:",e|n)
+# set intersection operation
+print("intersection of e and n:",e&n)
+# set difference operation
+e={0,8,6,1,2,3,4,5}
+n={1,2,3,4,5}
+print("difference of e and n:",e-n)
+# set symmetric difference operation
+e={0,1,2,3,4}
+n={4,5,6,8,2}
+print("symmetric difference of e and n:",e^n)
